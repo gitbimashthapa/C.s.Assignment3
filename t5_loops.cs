bbd184cfc89b12dfd519f8e27357ca1bc3ec9fb2
gitbimashthapa <mailbimashthapa@gmail.com>
2025-11-11 11:13:48 +0545

    Completed task 3

---
+++ t5_loops.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+
+//class Task5Program
+//{
+//    static void Main()
+//    {
+//        // For loop: Sum 1 to N
+//        Console.Write("Enter a number N: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        int sum = 0;
+
+//        for (int i = 1; i <= n; i++)
+//            sum += i;
+//        Console.WriteLine("Sum = " + sum);
+
+//        // While loop: Print 1–20, skip multiples of 4, stop at 15
+//        int num = 1;
+//        while (num <= 20)
+//        {
+//            if (num == 15) break;
+//            if (num % 4 == 0)
+//            {
+//                num++;
+//                continue;
+//            }
+//            Console.WriteLine(num);
+//            num++;
+//        }
+
+//        // Foreach loop: Sum of array elements
+//        int[] arr = { 2, 4, 6, 8 };
+//        int total = 0;
+//        foreach (int val in arr)
+//            total += val;
+//        Console.WriteLine("Array Sum = " + total);
+//    }
+//}
